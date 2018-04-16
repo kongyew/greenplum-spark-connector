2d8452fd40eb43b5336accf8e7b3446d4b1d6222
kongyew <kongyew@gmail.com>
2018-04-16 12:00:43 -0700

    Updated to use GSC 1.3

--- scripts/download_greenplum-spark-connector.sh
+++ scripts/download_greenplum-spark-connector.sh
@@ -13,9 +13,11 @@ cd `dirname $0`
 # Replace me with a valid Pivotal network token .  It is found under your profile ->PIVNET_TOKEN  (Link: https://network.pivotal.io/users/dashboard/edit-profile)
 # if you have pivnet cli , check cat ~/.pivnetrc
 export PIVNET_TOKEN=CHANGE_ME
-export PIVNET_TOKEN=H9sn5XbmcKWySySPhNxv
 
+
+# GSC 1.3
 export LOCAL_FILE_NAME=greenplum-spark_2.11-1.3.0.jar
+
 export DOWNLOAD_URL=https://network.pivotal.io/api/v2/products/pivotal-gpdb/releases/73874/product_files/111950/download
 wget -O "$LOCAL_FILE_NAME" --post-data="" --header="Authorization: Token $PIVNET_TOKEN" $DOWNLOAD_URL
 

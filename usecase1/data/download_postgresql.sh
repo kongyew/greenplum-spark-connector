038edf194a177f8904977fbcbc3bae21057a45a1
kongyew <kongyew@gmail.com>
2018-10-09 21:53:34 -0700

    updated

--- usecase1/data/download_postgresql.sh
+++ usecase1/data/download_postgresql.sh
@@ -5,6 +5,9 @@ current=`pwd`
 cd `dirname $0`
 
 # https://jdbc.postgresql.org/download.html
-wget -O postgresql-42.2.2.jar https://jdbc.postgresql.org/download/postgresql-42.2.2.jar
+#wget -O postgresql-42.2.2.jar https://jdbc.postgresql.org/download/postgresql-42.2.2.jar
+
+wget -O postgresql-42.2.5.jar https://jdbc.postgresql.org/download/postgresql-42.2.5.jar
+
 
 cd $current
